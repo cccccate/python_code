5c2a5a253d942dc5f3ff13b831806c8d5875b2ef
ccate <39542597+cccccate@users.noreply.github.com>
2018-12-03 21:20:23 +0800

    Update read_data_to_array.py

--- read_data_to_array.py
+++ read_data_to_array.py
@@ -13,10 +13,10 @@ def read_excel():
     sheet1 = workbook.sheet_by_name('Sheet1')
 
     # 获取整列的值（数组）
-    cols2 = sheet1.col_values(3)  # 获取第4列内容（联系方式）
-    cols1 = sheet1.col_values(0)  # 获取第1列内容（备注名）
+    cols2 = sheet1.col_values(1)  # 获取第2列内容
+    cols1 = sheet1.col_values(0)  # 获取第1列内容
 
-    # 输出第一列第四列内容（第四列联系方式以整数形式输出）
+    # 输出第一列第二列内容（第二列以整数形式输出）
     print(list(map(int, cols2)))
     # print(cols2)
     print(cols1)
